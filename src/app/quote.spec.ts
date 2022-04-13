5d693ebe8ebc45089afd95fcff91058b9ab72d28
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-13 10:59:17 +0300

    "Feat: added the typescript for quote class"

--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -2,6 +2,7 @@ import { Quote } from './quote';
 
 describe('Quote', () => {
   it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    expect(new Quote("","","",0)).toBeTruthy();
   });
 });
+
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
+    showDescription: boolean;
+constructor(public name: string,public author: string,public description: string, public datecount: number){
+  this.showDescription=false;
 }
+}
+
