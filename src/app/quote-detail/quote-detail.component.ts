a5e6d4e88539f37a8fd2b3ab3a121148e3bff31f
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-13 11:03:07 +0300

    "Feat: typescript for quote-detail component"

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,11 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
+
 export class QuoteDetailComponent implements OnInit {
+
+
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  quoteDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+
+  likes: number = 0;
+  dislikes: number = 0;
+
+  numberOfLikes(likes:any){
+    this.likes++;
+  }
+  numberOfDislikes(dislikes:any){
+    this.dislikes++;
+  }
+
+  showDetails:boolean= false;
+  toggle(){
+
+    this.showDetails= !this.showDetails;
+  }
+
+ 
+
 
   constructor() { }
 
@@ -13,3 +46,4 @@ export class QuoteDetailComponent implements OnInit {
   }
 
 }
+
