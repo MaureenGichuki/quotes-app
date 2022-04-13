33cef5a928a3c9063dbd021ef569250a332a4d85
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-13 10:58:51 +0300

    "Feta: added the datecount pipe code"

--- src/app/datecount.pipe.ts
+++ src/app/datecount.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'datecount'
+ 
 })
 export class DatecountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, args?:any): any{
+    
+    let start = new Date().getTime();
+    let end = new Date().getTime();
+    let time = end - start;
+    if(value>1){ 
+  
+      const min = Math.floor(((time % 86400000)%3600000)/6000)
+      
+    }else{
+      return 0
+    }
   }
-
-}
+  }
