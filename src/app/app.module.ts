2ea66862b9713f07e11661a489f437ebb4bcf9ed
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-13 10:07:23 +0300

    "adds declarations and imports of addes files"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DatecountPipe } from './datecount.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    DatecountPipe
   ],
   imports: [
     BrowserModule,
